d0b8c57ac1c5b34c94d464b4f6d97b8b5c82028b
Mirza Sharmin <mirza.sharmin1@gmail.com>
2021-12-13 04:17:41 +0700

    Initial commit

---
+++ app/src/main/java/com/sharmin/posapplication/LoginActivity.kt
@@ -0,0 +1,49 @@
+package com.sharmin.posapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.myapplication.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val headlineTextView = findViewById<TextView>(R.id.headline)
+        val userInput = findViewById<EditText>(R.id.userInput)
+        val passwordInput = findViewById<EditText>(R.id.passwordInput)
+        val button = findViewById<Button>(R.id.submitBtn)
+
+        headlineTextView.setOnLongClickListener {
+            navigateToSettingActivity()
+            true
+        }
+
+        button.setOnClickListener {
+            showToastMessage()
+            //navigateToOrderActivity()
+        }
+    }
+
+    private fun navigateToSettingActivity() {
+        startActivity(Intent(this, SettingActivity::class.java))
+    }
+
+    private fun navigateToOrderActivity() {
+        startActivity(Intent(this, OrderActivity::class.java))
+    }
+
+    private fun showToastMessage() {
+        val msg = "Sharmin has clicked the login button"
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+    }
+}
---
+++ app/src/main/java/com/sharmin/posapplication/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.sharmin.posapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
