4dd518156924d194f16b193fad31fdb089bbaf8c
Mirza Sharmin <mirza.sharmin1@gmail.com>
2021-12-13 04:22:57 +0700

    Updated packages

--- app/src/main/java/com/sharmin/posapplication/LoginActivity.kt
+++ app/src/main/java/com/sharmin/posapplication/LoginActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
-import com.example.myapplication.R
 
 class LoginActivity : AppCompatActivity() {
 
--- app/src/main/java/com/sharmin/posapplication/SettingActivity.kt
+++ app/src/main/java/com/sharmin/posapplication/SettingActivity.kt
@@ -2,7 +2,6 @@ package com.sharmin.posapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapplication.R
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
