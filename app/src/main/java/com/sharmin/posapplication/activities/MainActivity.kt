3cb924535fef98b04c3d898512d47c2ba5533586
Mirza Sharmin <mirza.sharmin1@gmail.com>
2021-12-23 14:24:05 +0700

    Midterm submission

--- app/src/main/java/com/sharmin/posapplication/activities/MainActivity.kt
+++ app/src/main/java/com/sharmin/posapplication/activities/MainActivity.kt
@@ -3,17 +3,20 @@ package com.sharmin.posapplication.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.MotionEvent
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.sharmin.posapplication.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
         setListeners()
     }
 
@@ -23,15 +26,31 @@ class LoginActivity : AppCompatActivity() {
         val passwordInput = findViewById<EditText>(R.id.passwordInput)
         val button = findViewById<Button>(R.id.submitBtn)
 
-        headlineTextView.setOnLongClickListener {
+        headlineTextView.setCustomLongClickListener(6000) {
             navigateToSettingActivity()
-            true
         }
 
         button.setOnClickListener {
-            showToastMessage()
-            //navigateToOrderActivity()
+            //showToastMessage()
+            navigateToOrderActivity()
         }
+    }
+
+    fun View.setCustomLongClickListener(clickDuration: Long, listener: () -> Unit) {
+        setOnTouchListener(object : View.OnTouchListener {
+
+            private val longClickDuration = clickDuration
+            private val handler = Handler()
+
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if (event?.action == MotionEvent.ACTION_DOWN) {
+                    handler.postDelayed({ listener.invoke() }, longClickDuration)
+                } else if (event?.action == MotionEvent.ACTION_UP) {
+                    handler.removeCallbacksAndMessages(null)
+                }
+                return true
+            }
+        })
     }
 
     private fun navigateToSettingActivity() {
--- app/src/main/java/com/sharmin/posapplication/activities/OrderActivity.kt
+++ app/src/main/java/com/sharmin/posapplication/activities/OrderActivity.kt
@@ -2,9 +2,14 @@ package com.sharmin.posapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.sharmin.posapplication.R
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 class OrderActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
