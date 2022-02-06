d8f874dfde80eb5f33e28b0e21201485d439fdda
ashfaq1701 <ashfaq.aws@gmail.com>
2022-02-07 06:28:07 +0700

    Take photo

--- app/src/main/java/com/sharmin/posapplication/screens/settings/SettingActivity.kt
+++ app/src/main/java/com/sharmin/posapplication/screens/settings/SettingActivity.kt
@@ -1,14 +1,83 @@
 package com.sharmin.posapplication.screens.settings
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import com.sharmin.posapplication.BuildConfig
 import com.sharmin.posapplication.R
+import com.sharmin.posapplication.databinding.ActivitySettingBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
 
 @AndroidEntryPoint
 class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+    private var lastImageUri: Uri? = null
+
+    private val takePhotoAction = registerForActivityResult(ActivityResultContracts.TakePicture()) { isSuccess ->
+        if (isSuccess && lastImageUri != null) {
+            binding.imgPreview.setImageURI(lastImageUri)
+        }
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                takePhoto()
+            } else {
+                Toast.makeText(
+                    this.applicationContext,
+                    R.string.camera_error_message,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupListeners()
+    }
+
+    private fun requestPermissionAndTakePhoto() {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) -> {
+                takePhoto()
+            }
+            else -> {
+                requestPermissionLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun takePhoto() {
+        lastImageUri = getTmpFileUri()
+        takePhotoAction.launch(lastImageUri)
+    }
+
+    private fun setupListeners() {
+        binding.uploadDailyReport.setOnClickListener {
+            requestPermissionAndTakePhoto()
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
     }
 }
