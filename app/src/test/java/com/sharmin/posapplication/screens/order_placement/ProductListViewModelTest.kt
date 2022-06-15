07ce68b01df89015c413e5f897f8d0915655b2f6
Ashfaq Salehin <ashfaq.aws@gmail.com>
2022-06-15 21:24:46 +0700

    Added tests for viewmodel layer

--- app/src/test/java/com/sharmin/posapplication/screens/order_placement/ProductListViewModelTest.kt
+++ app/src/test/java/com/sharmin/posapplication/screens/order_placement/ProductListViewModelTest.kt
@@ -1,19 +1,69 @@
 package com.sharmin.posapplication.screens.order_placement
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.sharmin.posapplication.db.models.Product
 import com.sharmin.posapplication.db.models.ProductType
+import com.sharmin.posapplication.ext.getOrAwaitValue
 import com.sharmin.posapplication.repositories.CartRepository
 import com.sharmin.posapplication.repositories.ProductRepository
+import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+import kotlin.test.assertEquals
 
 class ProductListViewModelTest {
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
-    private val productRepository = mock<ProductRepository>()
-    private val cartRepository = mock<CartRepository>()
+    private lateinit var productRepository: ProductRepository
+    private lateinit var cartRepository: CartRepository
     private val productType = ProductType.DRINKS
-    val viewModel = ProductListViewModel(productRepository, cartRepository, productType)
+    private lateinit var viewModel: ProductListViewModel
+
+    private val products = listOf(
+        Product("Product 1", ProductType.DESSERTS, 100, 50, "img1.jpg"),
+        Product("Product 2", ProductType.DESSERTS, 10, 20, "img2.jpg")
+    )
+
+    @Before
+    fun setUp() {
+        val productsLiveData = MutableLiveData<List<Product>>().apply {
+            value = products
+        }
+        productRepository = mock()
+        cartRepository = mock()
+        whenever(productRepository.getProductsByType(productType)).thenReturn(productsLiveData)
+        viewModel = ProductListViewModel(productRepository, cartRepository, productType)
+    }
+
+    @Test
+    fun `products calls getProductsByType from productRepository with correct parameter`() {
+        val productsValue = viewModel.products.getOrAwaitValue()
+
+        verify(productRepository).getProductsByType(productType)
+        assertEquals(productsValue, products)
+    }
+
+    @Test
+    fun `addProductToCart call addItemToCart in cartRepository with correct parameters`() {
+        val product = Product("Product 3", ProductType.DESSERTS, 20, 30, "img3.jpg")
+
+        viewModel.addProductToCart(product)
+
+        verify(cartRepository).addItemToCart(product, 1)
+    }
+
+    @Test
+    fun `removeProductFromCart call reduceItemFromCart in cartRepository with correct parameters`() {
+        val product = Product("Product 3", ProductType.DESSERTS, 20, 30, "img3.jpg")
+
+        viewModel.removeProductFromCart(product)
+
+        verify(cartRepository).reduceItemFromCart(product, 1)
+    }
 }
